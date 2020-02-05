022929a1254afed225628ee407149436d6c12d99
haigv77 <haigv77@gmail.com>
2020-02-05 10:49:29 +0700

    add input manager & pool manager

---
+++ Assets/Scripts/CheckRaycast.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckRaycast : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider != null)
+                {
+                    hit.collider.enabled = false;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    // Bit shift the index of the layer (8) to get a bit mask
+    public LayerMask layerMask;
+    public GameObject cube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 1000, layerMask))
+            {
+                if (hit.collider != null)
+                {
+                //    Debug.Log(hit.point);
+                   cube.Spawn(hit.point + new Vector3(0.0f, 0.5f, 0.0f));
+                }
+            }
+        }
+    }
+}
