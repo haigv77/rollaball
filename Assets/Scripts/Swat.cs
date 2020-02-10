c32a978ca011de0cf874988525abe10c33b2b548
haigv77 <haigv77@gmail.com>
2020-02-10 09:36:58 +0700

    + add animtion disapear

---
+++ Assets/Scripts/Swat.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swat : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag ("Wall"))
+        {
+            animator.SetTrigger("swatHit");
+        }
+
+    }
+}
